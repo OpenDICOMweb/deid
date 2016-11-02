19d45eafa5b40b742006300d25231ee9cbc31e8f
Jim Philbin <jfphilbin@gmail.com>
2016-11-02 09:21:42 -0400

    Added private_db for Private Tag Database

--- private_db/cvs_to_json.dart
+++ private_db/cvs_to_json.dart
@@ -1,146 +1,88 @@
 // Copyright (c) 2016, Open DICOMweb Project. All rights reserved.
 // Use of this source code is governed by the open source license
 // that can be found in the LICENSE file.
-// Author: Jim Philbin <jfphilbin@gmail.edu> - 
-// See the AUTHORS file for other contributors.
-
-// Copyright (c) 2016, Open DICOMweb Project. All rights reserved.
-// Use of this source code is governed by the open source license
-// that can be found in the LICENSE file.
 // Author: Jim Philbin <jfphilbin@gmail.edu> -
 // See the AUTHORS file for other contributors.
 
+import 'dart:convert';
 import 'dart:io';
 
-class CsvToDart {
-
-    CsvToDart();
-}
-
 void printLine(int i, var s) {
-    var no = i.toString().padLeft(4);
-    print('$no: $s');
+  var no = i.toString().padLeft(4);
+  print('$no: $s');
 }
+
 void main(List<String> args) {
-    var csvFile = "C:/odw/sdk/deid/private_db/new_db.csv";
-    var jsonFile = "C:/odw/sdk/deid/private_db/db_entry.json";
+  var csvFile = "C:/odw/sdk/deid/private_db/new1_db.csv";
+  var jsonFile = "C:/odw/sdk/deid/private_db/db_entry.json";
 
-    var inFile = new File(csvFile);
-    var lines = inFile.readAsLinesSync();
+  var inFile = new File(csvFile);
+  var lines = inFile.readAsLinesSync(encoding: SYSTEM_ENCODING);
 
-    var creator = lines.sublist(0, 1);
-    var data = lines.sublist(2);
+  var creatorJson = generateCreatorMap(lines);
+ // var dataJson = generateDataMaps(lines);
 
-    var creatorJson = generateCreatorJson(creator);
-    var dataJson = generateDataJson(creator);
-
-    //  var headLine = lines[0].split('|');
-    //  var headers = headLine.sublist(1);
-    //  for(String s in headers) s.trim();
-
-    List<List<String>> rows = [];
-    for (int i = 0; i < lines.length; i++) {
-        var line = lines[i];
-        printLine(i, line);
-        var row = lines[i].split('|');
-        row = row.sublist(1);
-        for (int j = 0; j < row.length; j++) {
-            row[j] = row[j].trim();
-        }
-        printLine(i, row);
-        rows.add(row);
-    }
-    for (int i = 0; i < rows.length; i++) {
-        printLine(i, rows[i]);
-    }
-
-    var generate = new PrivateGroupGenerator(rows);
-    print(generate.code);
+  var outFile = new File(jsonFile);
+  var json = JSON.encode(creatorJson);
+  outFile.writeAsStringSync(json);
+  print(creatorJson);
+  //print(dataJson);
 }
 
+List<String> splitAndTrim(String s, [String delimiter = "|"]) {
+  //print('String: $s');
+  List<String> list = s.split(delimiter);
+  List<String> out = [];
+  for (String s in list)
+    out.add(s.trim());
+  //print('trim: $out');
+  return out;
+}
 
-class PrivateGroupGenerator {
-    List<List<String>> table;
+List<Map<String, String>> generateDataMaps(List<String> lines) {
+  // Get and Clean Names
+  List<String> names = splitAndTrim(lines[0]);
+  print('data names: $names');
 
-    PrivateGroupGenerator(this.table);
-
-    List<String> get headers => table[0];
-    List<List<String>> get rows => table.sublist(1);
-
-    String get privateGroupHead(creator, manu)
-    var out = '''
-// Copyright (c) 2016, Open DICOMweb Project. All rights reserved.
-// Use of this source code is governed by the open source license
-// that can be found in the LICENSE file.
-// Original author: Jim Philbin <jfphilbin@gmail.edu> -
-// See the AUTHORS file for other contributors.
-
-import 'package:core/core.dart';
-
-enum Action {K, KB, R, X}
-
-const int private_group_base = 0x0009;
-const int creator_element_base = 0x0010;
-
-class PrivateGroup {
-  final String creator;
-  final String manufacturer;
-  final Modality modality;
-  final String description;
-  final PrivateData elements;
-
-  const PrivateGroup(this.creator, this.manufacturer, this.modality,
-                     this.description, this.elements);
-
-  int nextGroup(int group) => group + 2;
-
-  int nextCreator(int element) {
-    var next = element++;
-    return (inRange(0x0010, next, 0x00FF)) ? next : null;
+  List<Map<String, String>> maps = [];
+  for (int i = 1; i < lines.length; i++) {
+    List<String> values = splitAndTrim(lines[i]);
+  //  print('data values(${values.length}: $values');
+    Map<String, String> map = {};
+    for (int j = 0; j < values.length; j++) {
+  //    print('names[$j]: ${names[j]}, values[$j]: ${values[j]}');
+      map[names[j]] = values[j];
+  //    print('map: $map');
+    }
+    print('Final map: $map');
+    maps.add(map);
   }
-''';
-
-    String fields(creator) => '''
-  final String creator = $creator;
-  final int dGroup;
-  final int element;
-  final Modality modality;
-  final VR vr;
-  final Action action;
-  final String manufacturer;
-  final String description;
-
-  const PrivateGroup(this.creator, this.dGroup, this.element, this.modality,
-                     this.vr, this.action, this.manufacturer, this.description);
-
-''';
-
-    String get privateGroupDefinitions {
-        var out = "";
-        for (int i = 0; i < rows.length; i++) {
-            var row = rows[i];
-            var keyword = 'k${row[6]}';
-            var creator = '"${row[6]}"';
-            var mod = 'Modality.${row[2]}';
-            var manu = '"${row[5]}"';
-            var desc = '"${row[7]}"';
-            out += 'static const $keyword '
-                '= const PrivateGroup($creator, $manu, $mod, $desc);\n';
-        }
-        return out;
-    }
-
-    String get tail
-} => "\n}\n";
-
-
-
-String get code {
-    print('head: $head');
-    print('head: $definitions');
-    print('head: $tail');
-    return '$head$definitions$tail';
+  return maps;
 }
 
+
+Map generateCreatorMap(List<String> rows) {
+ //if (rows.length > 2) throw "Invalid Creator List";
+
+  // Clean identifiers
+  List<String> names = splitAndTrim(rows[0]);
+  print('creator names: $names');
+  List<String> values = splitAndTrim(rows[1]);
+  print('creator values: $values');
+
+  // Create Private Data Element Objects
+  if (names.length != values.length)
+    throw "Invalid - there must be the same number of names(${names.length} "
+        "and values(${values.length}";
+  Map<String, dynamic> map = {};
+  for (int i = 0; i < names.length; i++) map[names[i]] = values[i];
+  print('creator: $map');
+  List<Map> dataMaps = generateDataMaps(rows.sublist(2));
+  print('map0: $dataMaps');
+  map["elements"] = dataMaps;
+  print('creator: $map');
+  return map;
 }
+
+
 
